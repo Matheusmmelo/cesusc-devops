475a93a69b77e6113c00fbdaf5589a9ef5e40807
matheusmassaneiro <seuemail@exemplo.com>
2025-12-14 20:31:53 -0300

    chore: configura scripts e testes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = 3000;
 
-// Endpoint para servir a página HTML
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get('/', (req, res) => {
+  res.json({ message: 'API DevOps funcionando!' });
 });
 
-// Iniciar o servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}/index`);
-});
+module.exports = app;
