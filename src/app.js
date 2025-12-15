ea528fd8ea0a6e4a2ba98e68921b9873cb4f5f5e
matheusmassaneiro <seuemail@exemplo.com>
2025-12-15 01:03:10 -0300

    Atualiza mensagem da rota principal para teste de feature

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'API DevOps funcionando!' });
+  res.send('Servidor rodando! Atualização de teste na feature.');
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
 });
 
 module.exports = app;
